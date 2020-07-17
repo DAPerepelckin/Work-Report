ce0e48ac83c71eed5f6d433c53746ed7f517e4b1
Dmitry Perepelckin <DAPerepelckin@gmail.com>
2020-07-17 20:00:44 +0300

    10/06/2020

--- app/src/main/java/com/dapstd/workreport/ui/work_zone/WorkZoneFragment.java
+++ app/src/main/java/com/dapstd/workreport/ui/work_zone/WorkZoneFragment.java
@@ -1,51 +1,94 @@
 package com.dapstd.workreport.ui.work_zone;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.text.format.Time;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.dapstd.workreport.R;
+import com.dapstd.workreport.ui.work_zone.weekview.WeekView;
+import com.dapstd.workreport.ui.work_zone.weekview.WeekViewEvent;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
+import org.threeten.bp.LocalDate;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
+
+import static com.dapstd.workreport.ui.work_zone.weekview.WeekViewUtil.today;
+
 
 public class WorkZoneFragment extends Fragment {
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_work_zone, container, false);
 
-        Button addBtn = root.findViewById(R.id.add_work_button);
+        Button add = root.findViewById(R.id.add_work_button);
+        WeekView mWeekView = root.findViewById(R.id.weekView);
+        MaterialCalendarView calendarView = root.findViewById(R.id.calendarView);
 
-        RecyclerView workListView = root.findViewById(R.id.work_list_recycler_view);
-        workListView.setHasFixedSize(true);
-        List<Work> works = new ArrayList<>();
-        works.add(new Work("desctiprion"));
-        WorkListAdapter adapter = new WorkListAdapter(works);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(root.getContext());
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        workListView.setLayoutManager(linearLayoutManager);
-        workListView.setAdapter(adapter);
-        addBtn.setOnClickListener(v -> {
-            works.add(new Work("desctiprion"));
-            adapter.notifyDataSetChanged();
+
+        calendarView.setSelectedDate(LocalDate.now());
+        mWeekView.setSelectedDay(today());
+
+
+        mWeekView.setMonthChangeListener((newYear, newMonth) -> {
+            List<WeekViewEvent> events = new ArrayList<>();
+            return events;
         });
-        works.add(new Work("desctiprion"));
-        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
-        workListView.setItemAnimator(itemAnimator);
+
+        mWeekView.setXScrollingSpeed(0);
+
+        mWeekView.setEmptyViewClickListener(time -> {
+            if(mWeekView.getNumberOfVisibleDays()==7) {
+                mWeekView.setNumberOfVisibleDays(1);
+            }else{
+                mWeekView.setNumberOfVisibleDays(7);
+            }
+            mWeekView.goToDate(time);
+            mWeekView.setSelectedDay(time);
+            calendarView.setSelectedDate(LocalDate.of(time.getWeekYear(),time.getTime().getMonth()+1,time.getTime().getDate()));
+        });
+
+
+        calendarView.setOnDateChangedListener((widget, date, selected) -> {
+            if (calendarView.getCalendarMode() != CalendarMode.WEEKS) {
+                calendarView.state().edit().setCalendarDisplayMode(CalendarMode.WEEKS).commit();
+                mWeekView.setVisibility(View.VISIBLE);
+            }
+            mWeekView.goToDate(date.getDate());
+            mWeekView.setSelectedDay(new GregorianCalendar(date.getYear(), date.getMonth() - 1, date.getDay()));
+        });
+
+        calendarView.setOnTitleClickListener(v -> {
+            if(calendarView.getCalendarMode()==CalendarMode.WEEKS) {
+                calendarView.state().edit().setCalendarDisplayMode(CalendarMode.MONTHS).commit();
+                mWeekView.setVisibility(View.INVISIBLE);
+            }else{
+                calendarView.state().edit().setCalendarDisplayMode(CalendarMode.WEEKS).commit();
+                mWeekView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        calendarView.setOnMonthChangedListener((widget, date) -> {
+            mWeekView.goToDate(date.getDate());
+        });
+
         return root;
     }
 }
